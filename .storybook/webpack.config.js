a2bb02f92546a11bf351744b0562aed572a3a956
Maris Pukitis <mpukitis@rightpoint.com>
2019-02-14 15:54:02 -0700

    Addon Options for theming, Update Custom Webpack Config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,48 @@
 const path = require("path");
+const autoprefixer = require('autoprefixer');
 
 module.exports = (baseConfig, env, defaultConfig) => {
     // Extend defaultConfig as you need.
     
     // Sass loader
-    storybookBaseConfig.module.rules.push({
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "../")
-      });
+    // storybookBaseConfig.module.rules.push({
+    //     test: /\.scss$/,
+    //     loaders: ["style-loader", "css-loader", "sass-loader"],
+    //     include: path.resolve(__dirname, "../")
+    //   });
+
+    defaultConfig.module.rules.push(
+        {
+            test: /\.scss$/,
+            use: [
+                { 
+                    loader: 'style-loader' 
+                },
+                {
+                    loader: 'css-loader',
+                    options: { 
+                        importLoaders: 2 
+                    }
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: () => [
+                            autoprefixer({
+                                browsers: ['last 1 version', 'ie >= 11']
+                            })
+                        ]
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        includePaths: [ path.resolve(__dirname, '../node_modules') ]
+                    }
+                }
+            ]
+        }
+    );
          
     return defaultConfig;
   };
