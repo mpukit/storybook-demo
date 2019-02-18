89d491d383f24ad44b0acd5bd51c98745f515b3b
Maris Pukitis <mpukitis@rightpoint.com>
2019-02-18 14:41:45 -0700

    + Storybook Knobs Add-On, Cleanup Decorator Defaults

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,85 +1,15 @@
 import { addDecorator, configure } from '@storybook/html';
 import { withOptions } from '@storybook/addon-options';
+import { withKnobs } from '@storybook/addon-knobs';
 import { themes } from '@storybook/components';
 
 // Option defaults:
 addDecorator(
   withOptions({
-    /**
-     * name to display in the top left corner
-     * @type {String}
-     */
-    name: 'Storybook Demo', // ??
-    /**
-     * URL for name in top left corner to link to
-     * @type {String}
-     */
-    url: 'http://www.google.com', // ?
-    /**
-     * show story component as full screen
-     * @type {Boolean}
-     */
-    goFullScreen: false,
-    /**
-     * display panel that shows a list of stories
-     * @type {Boolean}
-     */
-    showStoriesPanel: true,
-    /**
-     * display panel that shows addon configurations
-     * @type {Boolean}
-     */
-    showAddonPanel: true,
-    /**
-     * display floating search box to search through stories
-     * @type {Boolean}
-     */
-    showSearchBox: false,
-    /**
-     * show addon panel as a vertical panel on the right
-     * @type {Boolean}
-     */
-    addonPanelInRight: false,
-    /**
-     * sorts stories
-     * @type {Boolean}
-     */
-    sortStoriesByKind: false,
-    /**
-     * regex for finding the hierarchy separator
-     * @example:
-     *   null - turn off hierarchy
-     *   /\// - split by `/`
-     *   /\./ - split by `.`
-     *   /\/|\./ - split by `/` or `.`
-     * @type {Regex}
-     */
-    hierarchySeparator: null,
-    /**
-     * regex for finding the hierarchy root separator
-     * @example:
-     *   null - turn off multiple hierarchy roots
-     *   /\|/ - split by `|`
-     * @type {Regex}
-     */
-    hierarchyRootSeparator: null,
-    /**
-     * sidebar tree animations
-     * @type {Boolean}
-     */
-    sidebarAnimations: true,
-    /**
-     * id to select an addon panel
-     * @type {String}
-     */
-    selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
-    /**
-     * enable/disable shortcuts
-     * @type {Boolean}
-     */
-    enableShortcuts: false, // true by default
-
-    theme: themes.dark,
+    name: 'Storybook Demo',
+    url: 'http://www.google.com',
+    addonPanelInRight: true,
+    //theme: themes.dark, // Enable Dark Theme
   })
 );
 
